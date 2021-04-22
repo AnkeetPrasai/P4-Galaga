213dfadc0a9d7ce10570e62ee3082f43893e0c25
Brandon Lefert <brandon.lefert@gmail.com>
2021-04-21 19:08:46 -0500

    Added stuff back to Leaderboard running

--- Galaga/Galaga.cpp
+++ Galaga/Galaga.cpp
@@ -265,6 +265,7 @@ void scoreScreen(Player & p)
     string scoreArray[5];
     string initialArray[5];
     string line;
+    string initial = "BBB";
     int i = 0;
 
     ifstream myfile("Leaderboard.txt");
@@ -286,13 +287,28 @@ void scoreScreen(Player & p)
         myfile.close();
     }
 
+    if (score > scoreArray[4])
+    {
+        scoreArray[4] = score;
+        initialArray[4] = initial;
+    }
+
     for (int i = 0; i < 5; i++)
     {
-        if (stoi(score) >= stoi(scoreArray[i]))
-        {
-            scoreArray[i] = score;
-            initialArray[i] = "BBB";
-        }
+            for (int j = i + 1; j < 5; j++)
+            {
+                if (stoi(scoreArray[i]) < stoi(scoreArray[j]))
+                {
+                    string tempScore;
+                    string tempInitial;
+                    tempScore = scoreArray[i];
+                    tempInitial = initialArray[i];
+                    scoreArray[i] = scoreArray[j];
+                    initialArray[i] = initialArray[j];
+                    scoreArray[j] = tempScore;
+                    initialArray[j] = tempInitial;
+                }
+            }
     }
 
     Text title("LEADERBOARD", font1, 60);
